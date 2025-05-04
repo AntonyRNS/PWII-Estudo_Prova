ce0678d8de4453b25dbfa7ac0d05fd9f61c9fbd2
AntonyRNS <antony.ryan.n.s@gmail.com>
2025-05-04 02:25:43 +0000

    v3.0(Final)

--- src/Codespaces/CatalogoProdutos/CatalogoProdutos.jsx
+++ src/Codespaces/CatalogoProdutos/CatalogoProdutos.jsx
@@ -1,67 +1,33 @@
 import { useState } from "react";
 
-
-
 function CatalogoProduto() {
-    const [produtos, setProdutos] = useState([
+    const [produtos] = useState([
         { id: 1, nome: "Notebook", preco: 3500, disponivel: true },
         { id: 2, nome: "Smartphone", preco: 2500, disponivel: false },
         { id: 3, nome: "Fone de Ouvido", preco: 300, disponivel: true },
         { id: 4, nome: "Monitor", preco: 1200, disponivel: false }
-    ])
-    const produtos_disponiveis = [produtos.filter(produto => produto.disponivel === true)]
+    ]);
 
-    const [visivel, setVisivel] = useState(true)
+    const [visivel, setVisivel] = useState(true);
+    // Não sabia que dava pra fazer isso, muito útil.
+    const produtosFiltrados = visivel ? produtos : produtos.filter(p => p.disponivel);
 
     return (
         <div>
-            <h3>Bem vindo(a)! Esses são todos nossos produtos: </h3>
-            {visivel
-                ? <ul style={{ color: 'red' }}>{produtos.map((produto) =>
-                    <li key='produto.id'>
-
-                        {produto.nome}
-
-                    </li>)}
-                </ul>
-
-                : <ul>{produtos.filter(produto => produto.disponivel === true).map((produto) =>
-                    <li key='produto.id'>
-
-                        {produto.nome}
-
-                    </li>)}
-                </ul>}
+            <h3>Bem-vindo(a)! Esses são nossos produtos:</h3>
             <ul>
-                {produtos.map(produto => {
-                    if (produto.disponivel === true) {
-                        return <li key={produto.id} style={{color:'green'}}>{produto.nome}</li>
-
-                    }else{
-                        return <li key={produto.id} style={{color:'red'}}>{produto.nome}</li>
-                    }
-
-
-                })}
+                {produtosFiltrados.map(produto => (
+                    // Renderização e estilização condicional com operador ternário, extremamente útil.
+                    <li key={produto.id} style={{ color: produto.disponivel ? 'green' : 'red' }}>
+                        {produto.nome}
+                    </li>
+                ))}
             </ul>
-
-
-
-
-            {visivel
-                ? <button onClick={() => setVisivel(!visivel)}>Mostrar apenas disponiveis</button>
-                : <button onClick={() => setVisivel(!visivel)}>Mostrar todos os produtos</button>}
-
-
+            <button onClick={() => setVisivel(!visivel)}>
+                {visivel ? 'Mostrar apenas disponíveis' : 'Mostrar todos os produtos'}
+            </button>
         </div>
-
-
     );
 }
 
-
-
-
-
 export default CatalogoProduto;
-
